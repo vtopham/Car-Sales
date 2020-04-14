70ddc67feb30e06beed117ce5cf43cb780c1ad43
Victoria  Topham <topham.victoria@gmail.com>
2020-04-14 10:22:05 -0700

    addedFeatures is connected

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 import AddedFeature from './AddedFeature';
 
+const mapPropsToState = state => {
+  return {
+    features: state.car.features
+  }
+}
 const AddedFeatures = props => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {props.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
+          {props.features.map(item => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
@@ -19,4 +25,4 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+export default connect(mapPropsToState, {})(AddedFeatures);
